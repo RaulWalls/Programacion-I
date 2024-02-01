5892e082307a576f3bcd9fbaeeebc27354648cf7
RaulWalls <60161477+RaulWalls@users.noreply.github.com>
2024-02-01 13:40:53 -0600

    Update and rename practica_1_1_Jorge_Oliver.cpp to practica_1_1.cpp

--- practica_1_1.cpp
+++ practica_1_1.cpp
@@ -7,13 +7,13 @@ int main()
     //se establecen las variables
     int num1, num2;
 
-    cout << "Programa que determina si un numero es múltiplo del otro." << endl;
+    cout << "Programa que determina si un numero es mÃºltiplo del otro." << endl;
     cout << "Dame el primer numero: " << endl;
     cin >> num1;
     cout << "Dame el segundo numero: " << endl;
     cin >> num2;
 
-    if(num1 % num2 == 0){   //se calcula el modulo entre ambos números y se compara con 0
+    if(num1 % num2 == 0){   //se calcula el modulo entre ambos nÃºmeros y se compara con 0
         cout<<"El numero "<<num2<<" es divisor de "<<num1;
     } else {
         cout<<"El numero "<<num2<<" no es divisor de "<<num1;
