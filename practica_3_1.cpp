6d2675d4928cbf4dca137dc96f66fbcee54f565c
RaulWalls <60161477+RaulWalls@users.noreply.github.com>
2024-02-01 13:42:17 -0600

    Update and rename practica_3_1_Jorge_Oliver.cpp to practica_3_1.cpp

--- practica_3_1.cpp
+++ practica_3_1.cpp
@@ -11,13 +11,13 @@ int main()
     for( int i=0; i<=10; i++){
         if(i>=5){        //compara la iteracion con la mitad del total de repeticiones para comenzar a descender
             var= "*";
-            for(int j=1; j<aux; j++){      //comienza un ciclo para imprimir la otra mitad del patrón de forma descendente
+            for(int j=1; j<aux; j++){      //comienza un ciclo para imprimir la otra mitad del patrÃ³n de forma descendente
                 cout << var;
             }
             aux--;                  //disminuye el valor de aux para imprimir un * menos
             cout << endl;
 
-        }else{                      // si la iteración i aun tiene un valor menor a cinco, continua aumentando un *
+        }else{                      // si la iteraciÃ³n i aun tiene un valor menor a cinco, continua aumentando un *
             cout << var << endl;
             var = var+"*";  //concatena el contenido de var agregando un * a la cadena
         }
